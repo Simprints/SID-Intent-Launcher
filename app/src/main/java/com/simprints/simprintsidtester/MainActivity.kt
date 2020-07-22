034fecaa3cbe6fac7bc7f0dbaddb5d86583759a7
Tsuharesu <tsuharesu01@gmail.com>
2020-07-22 11:54:17 +0100

    Use toolbar with navgraph

--- app/src/main/java/com/simprints/simprintsidtester/MainActivity.kt
+++ app/src/main/java/com/simprints/simprintsidtester/MainActivity.kt
@@ -2,17 +2,24 @@ package com.simprints.simprintsidtester
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.simprints.simprintsidtester.fragments.BackButtonInterface
 import com.simprints.simprintsidtester.fragments.list.IntentListFragment
 import com.simprints.simprintsidtester.fragments.list.IntentListFragmentDirections.Companion.openEditIntentFragment
 import com.simprints.simprintsidtester.model.domain.SimprintsIntent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), IntentListFragment.UserListActions {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onListFragmentInteraction(intent: SimprintsIntent) {
@@ -28,7 +35,7 @@ class MainActivity : AppCompatActivity(), IntentListFragment.UserListActions {
     }
 
     private fun openIntentDetails(intent: SimprintsIntent) {
-        openEditIntentFragment(intent).also { findNavController(this, R.id.nav_host_fragment).navigate(it) }
+        openEditIntentFragment(intent).also { findNavController(R.id.nav_host_fragment).navigate(it) }
     }
 
     override fun onBackPressed() {

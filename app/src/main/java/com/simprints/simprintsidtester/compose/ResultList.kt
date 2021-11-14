b434c9d65c880ddadf5497132b3b427a9a2141c9
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-11-14 19:47:39 +0300

    break down result list to account for nullability

--- app/src/main/java/com/simprints/simprintsidtester/compose/ResultList.kt
+++ app/src/main/java/com/simprints/simprintsidtester/compose/ResultList.kt
@@ -1,19 +1,29 @@
 package com.simprints.simprintsidtester.compose
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import com.google.android.material.composethemeadapter.MdcTheme
+import androidx.compose.ui.unit.dp
+import com.simprints.simprintsidtester.fragments.result.ResultListViewModel
 import com.simprints.simprintsidtester.model.domain.SimprintsResult
 
 @Composable
-fun ResultList(resultList: List<SimprintsResult>, modifier: Modifier = Modifier) {
-    MdcTheme {
-        LazyColumn {
-            items(items = resultList) { item ->
-                ResultItem(resultInfo = item, modifier)
-            }
+fun ResultList(resultListViewModel: ResultListViewModel) {
+    val resultList by resultListViewModel.getResults().observeAsState()
+    resultList?.let {
+        ResultListContent(it)
+    }
+}
+
+@Composable
+private fun ResultListContent(resultList: List<SimprintsResult>) {
+    LazyColumn {
+        items(items = resultList) { item ->
+            ResultItem(resultInfo = item, modifier = Modifier.padding(start = 16.dp, end = 16.dp))
         }
     }
 }

8589261eefd12bdca937aba52a807b0051ecac0a
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-11-14 20:18:56 +0300

    fix koin crash

--- app/src/main/java/com/simprints/simprintsidtester/SimprintsApplication.kt
+++ app/src/main/java/com/simprints/simprintsidtester/SimprintsApplication.kt
@@ -5,12 +5,13 @@ import com.simprints.simprintsidtester.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class SimprintsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(Level.NONE)
             androidContext(this@SimprintsApplication)
             modules(listOf(appModule))
         }
